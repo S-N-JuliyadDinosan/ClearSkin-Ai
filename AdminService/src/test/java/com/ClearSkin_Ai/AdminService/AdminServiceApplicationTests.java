09e73b5d55adba4ca9919dcee453ec005b95f67b
S N Juliyad Dinosan <brackdino90@gmail.com>
2025-07-01 11:49:44 +0530

    Admin service initiated and added dependencies

---
+++ AdminService/src/test/java/com/ClearSkin_Ai/AdminService/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ClearSkin_Ai.AdminService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
