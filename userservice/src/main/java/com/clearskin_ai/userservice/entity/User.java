db0c67e72338fe5f95b6f46d2b67ada5f72d78db
S N Juliyad Dinosan <brackdino90@gmail.com>
2025-07-02 14:54:13 +0530

    Entites created for user Service

---
+++ userservice/src/main/java/com/clearskin_ai/userservice/entity/User.java
@@ -0,0 +1,25 @@
+package com.clearskin_ai.userservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String email;
+    private String password;
+    private String name;
+    private Timestamp createdAt;
+}
